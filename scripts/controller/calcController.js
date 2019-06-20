fec425158b9bf8c3c55a14dba535c78dbd000d10
Ramon Almeida <ramondeallima@gmail.com>
2019-06-19 22:18:24 -0300

    Método que permite a inserção de vários eventos a um elemento

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,37 +1,62 @@
 class CalcController{
     
     constructor(){
-        this.locale = 'pt-BR';
-        this._timeEL = document.querySelector("#hora");
-        this._dateEL = document.querySelector("#data");
-        this._displayCalcEL = document.querySelector("#display");
+        this.locale = 'pt-BR'
+        this._timeEL = document.querySelector("#hora")
+        this._dateEL = document.querySelector("#data")
+        this._displayCalcEL = document.querySelector("#display")
 
-        this.currentDate;
-        this.initialize();
-        this.initButtonsEvents();
+        this.currentDate
+        this.initialize()
+        this.initButtonsEvents()
     }
 
     initialize(){
 
-        this.setDisplayDateTime();
+        this.setDisplayDateTime()
 
         setInterval(() => {
-            this.setDisplayDateTime();
-        }, 1000);
+            this.setDisplayDateTime()
+        }, 1000)
 
     }
 
+    // Método que de fato insere cada evento passado
+    // ao elemento através de um forEach que reconhece
+    // cada evento que fora passado como string mas transformado
+    // em Array pelo split.
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+            
+            element.addEventListener(event, fn, false)
+
+        })
+
+    }
+
+    // Método responsável por detectar todos os botões e texts
+    // com a tag "g" e também por acionar o método "addEventListenerAll"
+    // para inserir todos os eventos passados como parâmetro ao elemento.
     initButtonsEvents(){
 
-        let buttons = document.querySelectorAll('#buttons > g , #parts > g');
+        let buttons = document.querySelectorAll('#buttons > g , #parts > g')
 
         buttons.forEach((btn, index) =>{
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
+
                 console.log(btn.className.baseVal.replace("btn-", ""))
-            });
-    
-        });
+            
+            })
+            
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+                
+                btn.style.cursor = 'pointer'
+                
+            })
+        })
+        
     }
 
     setDisplayDateTime(){
@@ -39,36 +64,36 @@ class CalcController{
             day: "2-digit",
             month: "long",
             year: "numeric"
-        });
-        this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+        })
+        this.displayTime = this.currentDate.toLocaleTimeString(this.locale)
     }
 
     get displayCalc(){
-        return this._displayCalcEL.innerHTML;
+        return this._displayCalcEL.innerHTML
     }
     set displayCalc(value){
-        this._displayCalcEL.innerHTML = value; 
+        this._displayCalcEL.innerHTML = value 
     }
 
     get displayTime(){
-        return this._timeEL.innerHTML;
+        return this._timeEL.innerHTML
     }
     set displayTime(value){
-        return this._timeEL.innerHTML = value;
+        return this._timeEL.innerHTML = value
     }
 
     get displayDate(){
-        return this._dateEL.innerHTML;
+        return this._dateEL.innerHTML
     }
     set displayDate(value){
-        return this._dateEL.innerHTML = value;
+        return this._dateEL.innerHTML = value
     }
 
     get currentDate(){
-        return new Date;
+        return new Date
     }
     set currentDate(value){
-        this._dateEL.innerHTML = value; 
+        this._dateEL.innerHTML = value 
     }
 
 }

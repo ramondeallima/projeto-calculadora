adc7276a8e03b7bc6cc91f33d4b78504de1954f4
Ramon Almeida <ramondeallima@gmail.com>
2019-06-20 21:41:45 -0300

    Adicionado métodos de captura de entrada para números e operadores.

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,8 @@
 class CalcController{
     
     constructor(){
+
+        this._operation = [];
         this.locale = 'pt-BR'
         this._timeEL = document.querySelector("#hora")
         this._dateEL = document.querySelector("#data")
@@ -35,6 +37,72 @@ class CalcController{
 
     }
 
+    clearAll(){
+        this._operation = []
+    }
+
+    clearEntry(){
+        this._operation.pop()
+    }
+    
+    addOperation(value){
+
+        this._operation.push(value)
+
+        console.log(this._operation)
+
+    }
+
+    setError(){
+        this.displayCalc = 'Error';
+    }
+
+    
+    execBtn(value){
+        switch (value) {
+            case 'ac':
+                this.clearAll()
+                break;
+            case 'ce':
+                this.cancelEntry()  
+                break;
+            case 'soma':
+                
+                break;
+            case 'subtracao':
+                
+                break;
+            case 'divisao':
+                
+                break;
+            case 'multiplicacao':
+                
+                break;
+            case 'porcento':
+                
+                break;
+            case 'igual':
+                
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+                break
+            default:
+                this.setError()
+                break;
+        }
+    }
+
+
     // Método responsável por detectar todos os botões e texts
     // com a tag "g" e também por acionar o método "addEventListenerAll"
     // para inserir todos os eventos passados como parâmetro ao elemento.
@@ -46,7 +114,9 @@ class CalcController{
 
             this.addEventListenerAll(btn, 'click drag', e => {
 
-                console.log(btn.className.baseVal.replace("btn-", ""))
+                let textBtn = btn.className.baseVal.replace("btn-", "")
+                
+                this.execBtn(textBtn)
             
             })
             
